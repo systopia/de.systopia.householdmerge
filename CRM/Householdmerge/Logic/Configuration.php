systopia/de.systopia.householdmerge

commit f87218165a7c47d7d49f5516d0b137b3a68371c7

[#12] updating code base

CRM/Householdmerge/Logic/Configuration.php
@@ -30,9 +30,9 @@ class CRM_Householdmerge_Logic_Configuration {
    */
   public static function getSettings() {
     if (self::$settings_bucket === NULL) {
-      self::$settings_bucket = CRM_Core_BAO_Setting::getItem(self::$HHMERGE_SETTING_DOMAIN, 'householdmerge');
+      self::$settings_bucket = Civi::settings()->get('householdmerge');
       if (!is_array(self::$settings_bucket)) {
-        self::$settings_bucket = array();
+        self::$settings_bucket = [];
       }
     }
     return self::$settings_bucket;
@@ -211,7 +211,7 @@ class CRM_Householdmerge_Logic_Configuration {
    */
   public static function getLiveActivityStatusIDs() {
     if (self::$live_activity_status_ids === NULL) {
-      $status_ids = array();
+      $status_ids = [];
       $status_ids[] = CRM_Core_OptionGroup::getValue('activity_status', 'Scheduled', 'name');
       $status_ids[] = CRM_Core_OptionGroup::getValue('activity_status', 'Not Required', 'name');
       self::$live_activity_status_ids = implode(',', $status_ids);
@@ -227,7 +227,7 @@ class CRM_Householdmerge_Logic_Configuration {
    */
   public static function getFixableActivityStatusIDs() {
     if (self::$fixable_activity_status_ids === NULL) {
-      $status_ids = array();
+      $status_ids = [];
       $status_ids[] = CRM_Core_OptionGroup::getValue('activity_status', 'Scheduled', 'name');
       self::$fixable_activity_status_ids = implode(',', $status_ids);
     }

CRM/Admin/Form/Setting/Household.php
@@ -15,6 +15,8 @@
 
 require_once 'CRM/Core/Form.php';
 
+use CRM_Householdmerge_ExtensionUtil as E;
+
 /**
  * Form controller class
  *
@@ -22,30 +24,30 @@ require_once 'CRM/Core/Form.php';
  */
 class CRM_Admin_Form_Setting_Household extends CRM_Admin_Form_Setting {
   public function buildQuickForm() {
-    // load realtionships
+    // load relationships
     $relationshipOptions = $this->getEligibleRelationships();
 
     $this->addElement('select',
                     'hh_mode',
-                    ts('Household Mode', array('domain' => 'de.systopia.householdmerge')),
+                    E::ts('Household Mode'),
                     CRM_Householdmerge_Logic_Configuration::getHouseholdModeOptions(),
                     array('class' => 'crm-select2 huge'));
 
     $this->addElement('select',
                     'hh_head_mode',
-                    ts('Household Head Mode', array('domain' => 'de.systopia.householdmerge')),
+                    E::ts('Household Head Mode'),
                     CRM_Householdmerge_Logic_Configuration::getHouseholdHeadModeOptions(),
                     array('class' => 'crm-select2 huge'));
 
     $this->addElement('select',
                     'hh_member_relation',
-                    ts('Household Member Relationship', array('domain' => 'de.systopia.householdmerge')),
+                    E::ts('Household Member Relationship'),
                     $relationshipOptions,
                     array('class' => 'crm-select2 huge'));
 
     $this->addElement('select',
                     'hh_head_relation',
-                    ts('Household Head Relationship', array('domain' => 'de.systopia.householdmerge')),
+                    E::ts('Household Head Relationship'),
                     $relationshipOptions,
                     array('class' => 'crm-select2 huge'));
 
@@ -85,7 +87,7 @@ class CRM_Admin_Form_Setting_Household extends CRM_Admin_Form_Setting {
    * load all Individual<->Household Relationships
    */
   protected function getEligibleRelationships() {
-    $relationship_types = array();
+    $relationship_types = [];
 
     $list_ab = civicrm_api3('RelationshipType', 'get', array('contact_type_a' => 'Individual', 'contact_type_b' => 'Household'));
     foreach ($list_ab['values'] as $index => $relationship_type) {

CRM/Householdmerge/Form/Task/Merge.php
@@ -24,9 +24,9 @@ class CRM_Householdmerge_Form_Task_Merge extends CRM_Contact_Form_Task {
 
   function buildQuickForm() {
     // first: sort out the contacts
-    $contacts      = array();
-    $households    = array();
-    $organisations = array();
+    $contacts      = [];
+    $households    = [];
+    $organisations = [];
 
     foreach ($this->_contactIds as $contact_id) {
       $contact = civicrm_api3('Contact', 'getsingle', array('id' => $contact_id));
@@ -136,11 +136,11 @@ class CRM_Householdmerge_Form_Task_Merge extends CRM_Contact_Form_Task {
    ************************************************/
 
   function calculatePatterns(&$contacts) {
-    $patterns = array();
+    $patterns = [];
 
     // first do some analysis
-    $first_names = array();
-    $last_names = array();
+    $first_names = [];
+    $last_names = [];
     $common_last = NULL;
     foreach ($contacts as $contact_id => $contact) {
       if (!empty($contact['first_name'])) {
@@ -188,7 +188,7 @@ class CRM_Householdmerge_Form_Task_Merge extends CRM_Contact_Form_Task {
 
 
     // transform patterns into selection list
-    $pattern_selection = array();
+    $pattern_selection = [];
     foreach ($patterns as $pattern) {
       $pattern_selection[$pattern] = $pattern;
     }

CRM/Householdmerge/Logic/Checker.php
@@ -81,7 +81,7 @@ class CRM_Householdmerge_Logic_Checker {
    *
    */
   function checkHousehold($household_id) {
-    $problems_identified = array();
+    $problems_identified = [];
 
     // load household
     $household = civicrm_api3('Contact', 'getsingle', array('id' => $household_id));
@@ -100,7 +100,7 @@ class CRM_Householdmerge_Logic_Checker {
 
     // HEAD related checks
     if ('hierarchy' == CRM_Householdmerge_Logic_Configuration::getHouseholdMode()) {
-      $heads = array();
+      $heads = [];
       foreach ($members as $member) {
         if ($member['hh_relation'] == 'head') {
           $heads[] = $member;
@@ -180,7 +180,7 @@ class CRM_Householdmerge_Logic_Checker {
     }
 
     // build a list of all members...
-    $member_ids = array();
+    $member_ids = [];
     foreach ($members as $member) {
       if (!in_array($member['id'], $member_ids)) {
         $member_ids[] = $member['id'];
@@ -263,8 +263,8 @@ class CRM_Householdmerge_Logic_Checker {
   protected function getMembers($household_id) {
     $member_relation_id = CRM_Householdmerge_Logic_Configuration::getMemberRelationID();
     $head_relation_id   = CRM_Householdmerge_Logic_Configuration::getHeadRelationID();
-    $head_ids   = array();
-    $member_ids = array();
+    $head_ids   = [];
+    $member_ids = [];
 
     // load the relationships (both ways)
     $query = array(
@@ -310,7 +310,7 @@ class CRM_Householdmerge_Logic_Checker {
       return $members;
     }
 
-    return array();
+    return [];
   }
 
 }

CRM/Householdmerge/Logic/Problem.php
@@ -67,7 +67,7 @@ class CRM_Householdmerge_Logic_Problem {
   /**
    * create a problem instance defined by the given code
    */
-  public static function createProblem($code, $household_id, $params = array()) {
+  public static function createProblem($code, $household_id, $params = []) {
     $problem_classes = self::getProblemClasses();
     if (isset($problem_classes[$code])) {
       return new CRM_Householdmerge_Logic_Problem($code, $household_id, $params);
@@ -106,7 +106,7 @@ class CRM_Householdmerge_Logic_Problem {
   protected static $live_activity_status_ids = NULL;
   protected static $activity_type_id = NULL;
 
-  protected function __construct($code, $household_id, $params = array()) {
+  protected function __construct($code, $household_id, $params = []) {
     $this->code = $code;
     $this->household_id = $household_id;
     $this->params = $params;
@@ -178,7 +178,7 @@ class CRM_Householdmerge_Logic_Problem {
     // $smarty->popScope();
 
     // compile activity
-    $activity_data = array();
+    $activity_data = [];
     $activity_data['subject']            = $this->getTitle();
     // $activity_data['details']            = $activity_content;
     $activity_data['activity_date_time'] = date("Ymdhis");

CRM/Householdmerge/Logic/Scanner.php
@@ -31,7 +31,7 @@ class CRM_Householdmerge_Logic_Scanner {
    *                       'contacts'     => array(int=>contact_data)
    */
   public function findNewHouseholds($count) {
-    $proposals = array();
+    $proposals = [];
 
     // first, find candidates
     $candidates = $this->findCandidates($count);
@@ -60,7 +60,7 @@ class CRM_Householdmerge_Logic_Scanner {
     $contact_ids   = explode('||', $signature['contact_ids']);
     $display_names = explode('||', $signature['display_names']);
     $gender_ids    = explode('||', $signature['gender_ids']);
-    $members = array();
+    $members = [];
     for ($i=0; $i < count($contact_ids); $i++) {
       $contact_id = $contact_ids[$i];
       $members[$contact_id] = array(
@@ -87,8 +87,8 @@ class CRM_Householdmerge_Logic_Scanner {
       'household_id'   => 0,
       'head_id'        => 0,
       'household_name' => '',
-      'member_ids'     => array(),
-      'contacts'       => array(),
+      'member_ids'     => [],
+      'contacts'       => [],
       'address'        => array('street_address'         => $signature['street_address'],
                                 'supplemental_address_1' => $signature['supplemental_address_1'],
                                 'supplemental_address_2' => $signature['supplemental_address_2'],
@@ -150,7 +150,7 @@ class CRM_Householdmerge_Logic_Scanner {
         ";
     }
 
-    $candidates = array();
+    $candidates = [];
     $scanner_sql = "
       SELECT *
         FROM (SELECT civicrm_contact.id AS contact_id,
@@ -207,7 +207,7 @@ class CRM_Householdmerge_Logic_Scanner {
     if ($method == 'topdonor2y_m') {
 
       // init donations array
-      $donations = array();
+      $donations = [];
       foreach ($members as $member_id => $member) {
         $donations[$member_id] = 0;
       }

CRM/Householdmerge/Logic/Worker.php
@@ -16,7 +16,7 @@
 class CRM_Householdmerge_Logic_Worker {
 
   /** will store information on the relation types */
-  protected $_relationID2fields = array();
+  protected $_relationID2fields = [];
 
   /** will store information on the location types to be used for the address */
   protected $_household_address_location_type_id = NULL;
@@ -110,7 +110,7 @@ class CRM_Householdmerge_Logic_Worker {
     if ($this->_household_address_location_type_id == NULL) {
       $types = civicrm_api3('LocationType', 'get', array('is_default' => 1));
       if ($types['count'] == 0) {
-        $types = civicrm_api3('LocationType', 'get', array());
+        $types = civicrm_api3('LocationType', 'get', []);
       }
       $type = reset($types['values']);
       $this->_household_address_location_type_id = $type['id'];

CRM/Householdmerge/Page/Mergeview.php
@@ -29,7 +29,7 @@ class CRM_Householdmerge_Page_Mergeview extends CRM_Core_Page {
 
     // extract IDs
     $household_id = (int) CRM_Utils_Array::value('hid', $_REQUEST);
-    $other_ids    = array();
+    $other_ids    = [];
     $oids = preg_split('#,#', CRM_Utils_Array::value('oids', $_REQUEST, ""));
     foreach ($oids as $oid) {
       $oid = (int) $oid;
@@ -48,7 +48,7 @@ class CRM_Householdmerge_Page_Mergeview extends CRM_Core_Page {
     // NOW: load all contacts
     $household = civicrm_api3('Contact', 'getsingle', array('id' => $household_id));
 
-    $other_contacts = array();
+    $other_contacts = [];
     foreach ($other_ids as $other_id) {
       $other_contact = civicrm_api3('Contact', 'getsingle', array('id' => $other_id));
       $other_contact['was_merged'] = (bool) !empty($other_contact['contact_is_deleted']);
@@ -62,9 +62,9 @@ class CRM_Householdmerge_Page_Mergeview extends CRM_Core_Page {
     $merge_complete = TRUE;
     foreach ($other_contacts as &$other_contact) {
       if ($other_contact['was_merged']) continue;
-      $cacheParams = array();
+      $cacheParams = [];
       $mode = 'safe';
-      $dupePairs = array();
+      $dupePairs = [];
       $dupePairs[] = array('srcID' => $other_contact['id'], 'dstID' => $household_id);
 
       $result = CRM_Dedupe_Merger::merge($dupePairs, $cacheParams, $mode, FALSE);

commit 35bcef9a5ff2a17445e6c2f99d8508dbe32a0b1c

[#12] fixed activity status lists, error introduced with f87218165a7c47d7d49f5516d0b137b3a68371c7

CRM/Householdmerge/Logic/Configuration.php
@@ -216,7 +216,7 @@ class CRM_Householdmerge_Logic_Configuration {
           'option_group_id' => 'activity_status',
           'name' => ['IN' => ["Scheduled", "Not Required"]],
       ]);
-      foreach ($activity_status_list as $activity_status) {
+      foreach ($activity_status_list['values'] as $activity_status) {
         $status_ids[] = $activity_status['value'];
       }
       self::$live_activity_status_ids = implode(',', $status_ids);
@@ -226,7 +226,7 @@ class CRM_Householdmerge_Logic_Configuration {
   }
 
   /**
-   * get the activty status IDs that are considered to be live and fixable
+   * get the activity status IDs that are considered to be live and fixable
    *
    * @return string  comma separated ids
    */
@@ -237,7 +237,7 @@ class CRM_Householdmerge_Logic_Configuration {
           'option_group_id' => 'activity_status',
           'name' => ['IN' => ["Scheduled"]],
       ]);
-      foreach ($activity_status_list as $activity_status) {
+      foreach ($activity_status_list['values'] as $activity_status) {
         $status_ids[] = $activity_status['value'];
       }
       self::$fixable_activity_status_ids = implode(',', $status_ids);